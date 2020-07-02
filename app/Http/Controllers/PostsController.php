999d20aa114e6c6e4184720184f7bc9017ab0944
Demis Struiksma <demis@thenextweb.com>
2020-07-02 15:56:55 +0200

    Display 404 page if post was not found

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,6 +12,10 @@ class PostsController extends Controller
 
         $post = \DB::table('posts')->where('slug', $slug)->first();
 
+        if(!$post){
+            abort(404);
+        }
+
         return view('post', [
             'post' => $post
         ]);
