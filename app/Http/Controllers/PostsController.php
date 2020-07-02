33dfb890a5d2658c775ec181d1aaf31373d8248b
Demis Struiksma <demis@thenextweb.com>
2020-07-02 16:02:51 +0200

    Include & use Post Eloquent model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
+use App\Post;
+
 class PostsController extends Controller
 {
     
     public function show($slug)
     {
 
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->first();
 
         if(!$post){
             abort(404);
