96d58d6ece971833c6780c714fcb163743faf401
Demis Struiksma <demis@thenextweb.com>
2020-07-06 16:09:02 +0200

    Add header section to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,7 @@
 
 @section ('content')
 
-<div id="header-wrapper">
-	<div id="header" class="container">
-		<div id="logo">
-			<h1><a href="#">SimpleWork</a></h1>
-		</div>
-		<div id="menu">
-			<ul>
-				<li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-				<li><a href="#" accesskey="2" title="">Our Clients</a></li>
-				<li><a href="#" accesskey="3" title="">About Us</a></li>
-				<li><a href="#" accesskey="4" title="">Careers</a></li>
-				<li><a href="#" accesskey="5" title="">Contact Us</a></li>
-			</ul>
-		</div>
-	</div>
+@section ('header')
 	<div id="header-featured">
 		<div id="banner-wrapper">
 			<div id="banner" class="container">
@@ -26,6 +12,8 @@
 		</div>
 	</div>
 </div>
+@endsection
+
 <div id="wrapper">
 	<div id="page" class="container">
 		<div id="content">
