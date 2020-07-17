8a1a8abe6327d0aa61e325517d6eacf84a6e1dfd
Demis Struiksma <demis@thenextweb.com>
2020-07-17 15:19:29 +0200

    Use path method for articles link to single article page

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,7 +8,7 @@
 			<ul class="style1">
 				@foreach ($article as $articles)
 				<li class="first">
-					<a href="/articles/{{ $articles->id }}"><h3>{{ $articles->title }}</h3></a>
+					<a href="{{ $articles->path() }}"><h2>{{ $articles->title }}</h2></a>
 					<p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
                     <p><a href="#">{{ substr($articles->body, 0, 100) }}...</a></p>
 				</li>
